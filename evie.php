db106466d1f7a89e8755b6487690b88277799329
benh75-alt <83784815+benh75-alt@users.noreply.github.com>
2023-04-15 14:47:21 +0100

    Add files via upload

---
+++ evie.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<?php
+  function checkPassword($password) {
+    if ($password == "1234") {
+    $block = "I wish I could write a poem <br>
+      Just like the one I sent you <br>
+      Or even similar to Rob's <br>
+      But I am not the writer I thought I would become in year 6 <br>
+      So I cannot adequately describe <br>
+      How pretty you look when you smile <br>
+      Or the laugh that brightens the room <br>
+      
+      Nothing beats the aura of contentment <br>
+        when you're drinking your morning coffee <br>
+        Or the passion in your voice when discussing Taylor <br>
+        I could go on but this is an attempt at a poem, not an essay <br>
+        As always, <br>
+        If I can make you as happy as you make me, <br>
+        Then we'll both be alright. <br>
+        <br>
+        Happy Birthday Evie <br>
+        I love you. <br>
+        <br>
+        - Written the night before I fly to Portugal <br>
+        I should be asleep, but I miss you.";
+    echo $block;
+      echo '<script>document.getElementById("secretText").innerHTML = block; 
+      document.getElementById("one").innerHTML = "";</script>';
+    } else {
+      echo '<script>document.getElementById("one").innerHTML = "Incorrect password. Please try again.";</script>';
+    }
+  }
+?>
+<html>
+  <head>
+    <title>Happy Birthday Evie</title>
+    <link href=pretty.css rel="stylesheet" type="text/css">
+      
+  </head>
+  <body>
+    <h1>Happy Birthday Evie</h1>
+    <p>Please enter the password to view the secret text:</p>
+      <form method="post" action="evie.php">
+    <input type="password" id="balls" name="balls">
+    <input type="submit" id="but" value="Submit">
+    <p id="one"></p>
+    <p id="secretText"></p>
+
+      </form>
+   <?php
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$password = $_POST['balls'];
+        checkPassword($password);
+      }
+          ?>
+  </body>
+</html>
